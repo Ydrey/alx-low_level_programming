0d1d3e8cfa9f243f32fd2e884dbd2b25e009d79b
Ydrey <ayobamiadedayo01@gmail.com>
2023-03-30 16:50:56 -0700

    open eye

--- main.h
+++ main.h
@@ -1,16 +1,19 @@
-#include "main.h"
-void positive_or_negative(int i);
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
 
-int main(void)
-{
-int i;
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
-i = 0;
-positive_or_negative(i);
-
-return (0);
-}}
+#endif
